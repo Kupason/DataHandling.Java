adddcf0bdae0e0b8f3f83492c71fa8a01c20b8f7
Kgaugelo Lj Kupa <109580059+Kupason@users.noreply.github.com>
2022-08-15 11:43:48 +0200

    Add files via upload

---
+++ dataHiding.java
@@ -0,0 +1,75 @@
+// Java Program showing working of data hiding
+
+// Importing generic libraries
+import java.io.*;
+
+// Class created named Bank
+class Bank {
+
+	// Private data (data hiding)
+	private long CurBalance = 0;
+
+	// Bank_id is checked for authentication
+	long bank_id;
+	String name;
+
+	// Getter function to modify private data
+	public long get_balance(long Id)
+	{
+
+		// Checking whether the user is
+		// authorised or unauthorised
+
+		// Comparing bank_id of user and the give Id
+		// then only it will get access
+		if (this.bank_id == Id) {
+
+			// Return current balance
+			return CurBalance;
+		}
+
+		// Unauthorised user
+		return -1;
+	}
+	// Setter function
+	public void set_balance(long balance, long Id)
+	{
+		// Comparing bank_id of user and the give Id
+		// then only it will get access
+		if (this.bank_id == Id) {
+			// Update balance in current ID
+			CurBalance = CurBalance + balance;
+		}
+	}
+}
+
+// Another class created- Employee
+public class Emp {
+	public static void main(String[] args)
+	{
+		// Creating employee object of bank type
+		Bank _emp = new Bank();
+
+		// Assigning employee object values
+		_emp.bank_id = 54231;
+		_emp.name = "kupa";
+
+		// _emp.get_balance(12345)
+		_emp.set_balance(50000, 12345);
+		// This will no get access as bank_id is given wrong
+		// so
+		// unauthorised user is not getting access that is
+		// data hiding
+
+		long emp_balance = _emp.get_balance(12345);
+		// As this time it is valid user it will get access
+
+		// Display commands
+		System.out.println("User Name"
+						+ " " + _emp.name);
+		System.out.println("Bank_ID"
+						+ " " + _emp.bank_id);
+		System.out.println("Current Balance"
+						+ " " + emp_balance);
+	}
+}
